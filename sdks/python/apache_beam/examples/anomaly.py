d2c489e18e1ff8dbb033d62109a56bc540bc7b27
Shunping Huang <shunping@google.com>
2024-11-05 09:44:12 -0500

    Add a simple anomly detection example.

---
+++ sdks/python/apache_beam/examples/anomaly.py
@@ -0,0 +1,62 @@
+import argparse
+import logging
+
+import apache_beam as beam
+from apache_beam.options.pipeline_options import PipelineOptions
+from apache_beam.options.pipeline_options import SetupOptions
+from apache_beam.ml.anomaly.transforms import AnomalyDetection
+from apache_beam.ml.anomaly.detectors import AnomalyDetector
+from apache_beam.ml.anomaly.thresholds import FixedThreshold
+
+
+def run(argv=None, save_main_session=True):
+  """
+  Args:
+    argv: Command line arguments defined for this example.
+    save_main_session: Used for internal testing.
+  """
+  parser = argparse.ArgumentParser()
+  known_args, pipeline_args = parser.parse_known_args(argv)
+
+  # We use the save_main_session option because one or more DoFn's in this
+  # workflow rely on global context (e.g., a module imported at module level).
+  pipeline_options = PipelineOptions(pipeline_args)
+  pipeline_options.view_as(SetupOptions).save_main_session = save_main_session
+
+  data = [
+      (1, beam.Row(x1=1, x2=4)),
+      (2, beam.Row(x1=100, x2=5)),  # an row with a different key (key=2)
+      (1, beam.Row(x1=2, x2=4)),
+      (1, beam.Row(x1=3, x2=5)),
+      (1, beam.Row(x1=10, x2=4)),  # outlier in key=1, with respect to x1
+      (1, beam.Row(x1=2, x2=10)),  # outlier in key=1, with respect to x2
+      (1, beam.Row(x1=3, x2=4)),
+  ]
+
+  detectors = []
+  detectors.append(
+      AnomalyDetector(
+          algorithm="SAD",
+          features=["x1"],
+          threshold_func=FixedThreshold(3),
+          id="sad_x1"))
+  detectors.append(
+      AnomalyDetector(
+          algorithm="SAD",
+          features=["x2"],
+          threshold_func=FixedThreshold(2),
+          id="sad_x2"))
+
+  # The pipeline will be run on exiting the with block.
+  with beam.Pipeline(options=pipeline_options) as p:
+    _ = (
+        p | beam.Create(data)
+        # TODO: get rid of this conversion between BeamSchema to beam.Row.
+        | beam.Map(lambda t: (t[0], beam.Row(**t[1]._asdict())))
+        | AnomalyDetection(detectors)
+        | beam.Map(logging.info))
+
+
+if __name__ == '__main__':
+  logging.getLogger().setLevel(logging.INFO)
+  run()
